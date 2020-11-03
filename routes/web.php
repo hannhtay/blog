3d58b62af362937e31cbc1ea46edda3faaa650f2
Hann Htay <hannhtay123@gmail.com>
2020-11-03 21:41:36 +0630

    File manager is added to upload file with editor

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+ Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web', 'auth']], function () {
+     \UniSharp\LaravelFilemanager\Lfm::routes();
+ });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
